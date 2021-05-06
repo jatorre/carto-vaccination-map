2aa07592c65671f3d8b725952f0f0d882422b112
alex-graciano <79551116+alex-graciano@users.noreply.github.com>
2021-05-06 13:35:51 +0200

    Update stats_points_tileset_query.sql

--- stats_points_tileset_query.sql
+++ stats_points_tileset_query.sql
@@ -1,38 +1,3 @@
---DROP TABLE IF EXISTS `cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp1`;
---CALL bqcarto.tiler.CREATE_POINT_AGGREGATION_TILESET(
---R'''(
---    SELECT * FROM `cartobq.maps.covid19_vaccinated_usa_blockgroups_100pct`
---)''',
---'`cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp1`',
---R'''
---    {
---    "zoom_min": 0,
---    "zoom_max": 14,
---    "aggregation_type": "quadkey",
---    "aggregation_resolution": 6,
---    "aggregation_placement": "features-centroid",
---    "properties":{
---        "aggregated_total": {
---        "formula":"count(*)",
---        "type":"Number"
---        },
---        "vaccinated": {
---        "formula":"SUM(IF(vaccinated = 'true', 1, 0))",
---        "type":"Number"
---        },
---        "non_vaccinated": {
---        "formula":"SUM(IF(vaccinated = 'false', 1, 0))",
---        "type":"Number"
---        },
---        "unknown": {
---        "formula":"SUM(IF(vaccinated = 'unknown', 1, 0))",
---        "type":"Number"
---        }
---    }
---    }
---'''
---    );
-
 DROP TABLE IF EXISTS `cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp`;
 CALL bqcarto.tiler.CREATE_POINT_AGGREGATION_TILESET(
 R'''(
@@ -91,7 +56,7 @@ R'''
 );
 
 -- Allocate the tileset in its final table
-DROP TABLE IF EXISTS `cartobq.maps.covid19_vaccinated_stats_usa_tileset`;
-CREATE OR REPLACE TABLE `cartobq.maps.covid19_vaccinated_stats_usa_tileset` AS 
-SELECT * FROM `cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp`;
-DROP TABLE IF EXISTS `cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp`;
+--DROP TABLE IF EXISTS `cartobq.maps.covid19_vaccinated_stats_usa_tileset`;
+--CREATE OR REPLACE TABLE `cartobq.maps.covid19_vaccinated_stats_usa_tileset` AS 
+--SELECT * FROM `cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp`;
+--DROP TABLE IF EXISTS `cartobq.maps.covid19_vaccinated_stats_usa_tileset_temp`;
